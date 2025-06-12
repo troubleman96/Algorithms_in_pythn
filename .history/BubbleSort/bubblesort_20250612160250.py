2b51ed4a60687e26095d62db3a242fb07f6a63e6
troubleman96 <itslugenge96@gmail.com>
2025-06-12 22:32:05 +0300

    Implementing Bubbe sort

---
+++ .history/BubbleSort/bubblesort_20250612160250.py
@@ -0,0 +1,16 @@
+def bubble_sort(sample):
+    n = len(sample)
+
+    for i in range(n):
+        for j in range(0, n - i - 1): ##this is the important inner loop
+            if sample[j] > sample[j + 1]:
+                #swap if eleements are in wrong orrder
+                sample[j], sample[j + 1] = sample[j +1], sample[j] ##shortcut for swapping in python
+    return sample            
+
+
+
+numbers = [5,3,8,4,2]
+sorted_sample = bubble_sort(numbers)
+print(sorted_sample)
+
---
+++ BubbleSort/bubblesort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1): ##this is the important inner loop
+            if arr[j] > arr[j + 1]:
+                #swap if eleements are in wrong orrder
+                arr[j], arr[j + 1] = arr[j +1], arr[j] ##shortcut for swapping in python
+    return arr            
+
+
+
+numbers = [5,3,8,4,2]
+sorted_sample = bubble_sort(numbers)
+print(sorted_sample)
+
